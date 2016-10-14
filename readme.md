387e2473aba6954b9996ec3479c72b1332830330
Juliet Berman <berman.juliet@gmail.com>
2016-10-13 23:28:32 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+Hangman User Story:
+- Views dashes for hidden word [bronze]
+- Select letter of alphabet (buttons) to guess if letter is in word [bronze]
+- If correct guess: display letter and disable the letter button [bronze]
+- If incorrect guess: pop up alert to "try again" & disable the letter button [bronze], and portion of hangman graphic is added [gold]
+- Play button to run against timer [silver]
+- Keeps track of incorrect guesses/score value [silver]
+- Can choose topic/theme option which will change word and give a whole new game [gold]
---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  console.log("hello jQuery");
+
+var hangman = {
+  storedWord: [],
+  dashes: [],
+  abc: []
+};
+
+// Click event on "Let's Play" button triggers: capturing input value, splitting value string into letters, storing letters in object array, clearing value input, clearing div textbox, and appending dashes for each letter
+
+$(".wordChoice").on("click", function(){
+  var word = $(".wordInput").val();
+  hangman.storedWord = word.split("");
+  $(".wordInput").val("");
+  var textBox = $("div.textBox")
+  textBox.html("")
+  for (i = 0; i< hangman.storedWord.length; i++) {
+    hangman.dashes[i] = "_";
+    }
+  textBox.append(hangman.dashes);
+  textBox.append("<p id='guess'>(take a guess!)</p>");
+  hangman.abc = "abcdefghijklmnopqrstuvwxyz".split("");
+  keyboard();
+});
+
+  // Function is called upon capturing value and updating object - creates letter buttons with specific ids. Values are stored in object
+
+function keyboard(){
+  for (i=0; i< hangman.abc.length; i++){
+    $("div.keyboard").append("<input type='button' value='" + hangman.abc[i].toUpperCase() + "' id='" + hangman.abc[i] + "' class='letter'>")
+  }
+};
+
+  function magic (){
+    //when click letter button loop through storedword to compare value. If match, dashes = storedWord[i]. Reprint
+  };
+
+
+// To star out letters as you type, to keep word hidden for player #2
+$(".wordInput").keyup(function(event) {
+hiddenText = event.target.value;
+// console.log(hiddenText);
+// $('.wordInput').text(hiddenText.replace(/\+/g, ' '));
+});
+
+console.log(hangman);
+
+});
