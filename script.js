21a6caed848b5a1e57185020a4470d74cac708d5
Juliet Berman <berman.juliet@gmail.com>
2016-10-14 21:23:05 -0400

    reworked functions and updated styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   console.log("hello jQuery");
 
 var hangman = {
@@ -7,49 +8,53 @@ var hangman = {
   abc: []
 };
 
-// Click event on "Let's Play" button triggers: capturing input value, splitting value string into letters, storing letters in object array, clearing value input, clearing div textbox, and appending dashes for each letter
-
 $(".wordChoice").on("click", function(){
+  captureWordValue();
+  createDashes();
+  keyboard();
+  updateText();
+  scoreBoard();
+});
+
+// INSERT CLICK EVENT FOR KEYPRESS
+
+function captureWordValue(){
   var word = $(".wordInput").val().toUpperCase();
   hangman.storedWord = word.split("");
+};
 
-
-  var textBox = $("div.textBox>h2");
-
+function createDashes(){
   for (var i = 0; i< hangman.storedWord.length; i++) {
     hangman.dashes[i] = "_";
     }
-// updating text box with dashes
-  textBox.append(hangman.dashes);
-  $("p").text("(take a guess!)");
-  $("div.input").html("");
+};
 
-// populating object with alphabet letters, populate keyboard
-  hangman.abc = "abcdefghijklmnopqrstuvwxyz".split("");
-  keyboard();
-
-// show scoreboard
-  $("div.scoreBoard").css("visibility", "visible");
-  $("div.scoreBoard").append("<p id='life'>Lives Remaining: 6</p><p id='points'>Points: 0 </p>");
-});
-
-// Function is called upon capturing value and updating object - creates letter buttons with specific ids. Values are stored in object
 function keyboard(){
+  hangman.abc = "abcdefghijklmnopqrstuvwxyz".split("");
   for (i=0; i< hangman.abc.length; i++){
-    $("div.keyboard").append("<input type='button' value='" + hangman.abc[i].toUpperCase() + "' id='" + hangman.abc[i] + "' class='letter'>")
+    $("div.keyboard").append("<input type='button' value='" + hangman.abc[i].toUpperCase() + "' class='letter'>")
   }
+  $("input.letter").on("click", checkMatch);
+};
 
-  // on click of button, compare its value to value stored in array. If match, then update dashes array at that same index. Reprint to textbox and disable button. If not match, reduce score on score board and disable button
-  $(".letter").on("click", function(){
+  function updateText(){
+    var textBox = $("div.textBox>h2");
+    textBox.append(hangman.dashes);
+    $("p").text("(take a guess!)");
+    $("div.input").html("");
+  };
+
+  function scoreBoard (){
+    $("div.scoreBoard").css("visibility", "visible");
+    $("div.scoreBoard").append("<p id='life'>Lives Remaining: 6</p><p id='points'>Points: 0 </p>");
+  };
+
+  function checkMatch(){
     var letter = $(this).val();
+    var x = "false";
 
-    if ($.inArray(letter, hangman.storedWord) > -1)
-    {
-
-    for (var i = 0; i < hangman.storedWord.length; i++)
-        {
-            if (hangman.storedWord[i] == letter)
-            {
+    for (var i = 0; i < hangman.storedWord.length; i++){
+            if (hangman.storedWord[i] == letter){
               // update dashes array with letter value and update div text
               hangman.dashes[i] = letter;
               $("div.textBox>h2").html(hangman.dashes);
@@ -63,32 +68,36 @@ function keyboard(){
               pointsText = pointsText.replace(/\D/g, '');
               var newPoints = parseFloat(pointsText) + 10;
               $("p#points").text("Points: " + newPoints);
+
+              x = "true";
+            }
+            else {
+              // remove clicked button
+              $(this).prop('disabled', true);
+              $(this).addClass('disabled').removeClass('letter');
             }
         };
-    }
-    else {
-      // remove clicked button
-      $(this).prop('disabled', true);
-      $(this).addClass('disabled').removeClass('letter');
 
-      // remove life
-      var lifeText = $("p#life").text();
-      lifeText = lifeText.replace(/\D/g, '');
-      var newLife = parseFloat(lifeText) - 1;
-      $("p#life").text("Lives Remaining: " + newLife);
-    }
-
-    });
-
+  if (x == "false"){
+    updateLifeScore();
+  }
 };
 
+function updateLifeScore (){
+  var lives = $("p#life").text();
+  lives = lives.replace(/\D/g, '');
+  var newLife = (parseFloat(lives) - 1);
+  $("p#life").text("Lives Remaining: " + newLife);
+};
+
+});
+
+// function draw (){
+//
+// };
 
 // // To star out letters as you type, to keep word hidden for player #2
 // $(".wordInput").keyup(function(event) {
 // hiddenText = event.target.value;
 // console.log(hiddenText);
 // $('.wordInput').text(hiddenText.replace(/\+/g, ' '));
-// });
-
-
-});
