aab8b06abdaadf0b5cfee1004f41eaad86f0273f
Juliet Berman <berman.juliet@gmail.com>
2016-10-17 10:07:59 -0400

    adds comments to code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 console.log("hello jQuery");
 
+// object stores all data - input word, dashes, keyboard letter values, and hangman images
 var hangman = {
   storedWord: [],
   dashes: [],
@@ -9,7 +10,7 @@ var hangman = {
   images: ["images/gallow1.png", "images/gallow2.png", "images/gallow3.png", "images/gallow4.png", "images/gallow5.png", "images/gallow6.png", "images/gallow7.png", "images/gallow8.png"]
 };
 
-
+// timer function includes global variable to access number of seconds later in point bonus function
 var seconds = 90;
 var countDown = null;
 
@@ -33,6 +34,7 @@ function timer(){
         }
 };
 
+// onclick to begin playing - references various, modular functions
 $(".wordChoice").on("click", function(){
 
   if ($(".wordInput").val() ==""){
@@ -48,17 +50,20 @@ $(".wordChoice").on("click", function(){
   }
 });
 
+// allows "Enter" key to invoke onclick play event
 $(".wordInput").keypress(function(e){
     if(e.which == 13){
       $(".wordChoice").click();
     }
 });
 
+// captures value from input box and stores in object
 function captureWordValue(){
   var word = $(".wordInput").val().toUpperCase();
   hangman.storedWord = word.split("");
 };
 
+// creates number of dashes based on length of word, stores in object
 function createDashes(){
   for (var i = 0; i< hangman.storedWord.length; i++) {
       if (hangman.storedWord[i] != " "){
@@ -70,6 +75,7 @@ function createDashes(){
     }
 };
 
+// creates keyboard with each key having its own click event
 function keyboard(){
   hangman.abc = "abcdefghijklmnopqrstuvwxyz".split("");
   for (i=0; i< hangman.abc.length; i++){
@@ -78,6 +84,7 @@ function keyboard(){
   $("input.letter").on("click", checkMatch);
 };
 
+// changes textbox to show dashes, takes info from the object
 function updateText(){
   var textBox = $("div.textBox>h2");
   textBox.append(hangman.dashes);
@@ -85,11 +92,13 @@ function updateText(){
   $("div.input").html("");
 };
 
+// displays scoreboard beginning with zero points
 function scoreBoard (){
   $("div.scoreBoard").css("visibility", "visible");
   $("div.scoreBoard").append("<p id='life'>Lives Remaining: 8</p><p id='points'>Points: 0 </p>");
 };
 
+// checks letter key with the word input stores in the object
 function checkMatch(){
   var letter = $(this).val();
   var x = "false";
@@ -102,10 +111,7 @@ function checkMatch(){
               $(this).prop('disabled', true);
               $(this).addClass('disabled').removeClass('letter');
 
-              var pointsText = $("p#points").text();
-              pointsText = pointsText.replace(/\D/g, '');
-              var newPoints = parseFloat(pointsText) + 10;
-              $("p#points").text("Points: " + newPoints);
+              addPoint();
 
               x = "true";
           }
@@ -122,6 +128,27 @@ function checkMatch(){
   checkWin();
 };
 
+// runs through to see if there are any more dashes stores in object
+function checkWin(){
+  if ($.inArray("_", hangman.dashes) < 0){
+    $("p#subtext").html("<span id='win'>YOU WIN!</span>");
+    $("input.letter").prop('disabled', true);
+    $("div.input").html("<button id='playagain'>Play again!</button>");
+    $("button#playagain").on("click", playAgain);
+    clearInterval(countDown);
+    bonusPoints();
+  }
+};
+
+// if letter match against stored word, adds 10 points per letter
+function addPoint(){
+    var pointsText = $("p#points").text();
+    pointsText = pointsText.replace(/\D/g, '');
+    var newPoints = parseFloat(pointsText) + 10;
+    $("p#points").text("Points: " + newPoints);
+};
+
+// if no match of letter in stored word, deducts live value and updates scoreboard
 function updateLifeScore (){
   var lives = $("p#life").text();
   lives = lives.replace(/\D/g, '');
@@ -141,6 +168,7 @@ function updateLifeScore (){
     }
 };
 
+// runs through each hangman image, invoked by loss of life or timer runs down
 var counter = 0;
 
 function hangingMan (){
@@ -148,28 +176,20 @@ function hangingMan (){
   counter ++;
 };
 
-function checkWin(){
-  if ($.inArray("_", hangman.dashes) < 0){
-    $("p#subtext").html("<span id='win'>YOU WIN!</span>");
-    $("input.letter").prop('disabled', true);
-    $("div.input").html("<button id='playagain'>Play again!</button>");
-    $("button#playagain").on("click", playAgain);
-    clearInterval(countDown);
-    bonusPoints();
-  }
-};
-
+// option to reveal answer, invoked by click event on button with loss
 function revealAnswer (){
   $("div.textBox>h2").html(hangman.storedWord);
   $("div.input").html("<button id='playagain'>Play again!</button>");
   $("button#playagain").on("click", playAgain);
 }
 
+// adds time bonus, invoked by click event on button with win
 function bonusPoints(){
   var bonus = seconds * 2;
   $("p#points").append(" + " + bonus + " time bonus")
 }
 
+// refreshes page to play again, invoked by click event on button
 function playAgain (){
   location.reload();
 }
